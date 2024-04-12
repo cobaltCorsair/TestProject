facfda98237f56b9f844c78b6c332e733ce871ae
cobaltCorsair <cobaltcorsair@yandex.ru>
2024-04-12 16:08:45 +0300

    Added migration

---
+++ Migrations/202404121307505_DBChanges.cs
@@ -0,0 +1,16 @@
+﻿namespace TestProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DBChanges : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
